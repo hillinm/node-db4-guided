cbd29584718f5c8f5597f6edfcd2f3a50fe9c831
Mark Hillin <mark.hillin@gmail.com>
2021-02-04 14:29:19 -0600

    Done

--- README.md
+++ README.md
@@ -9,9 +9,9 @@ Guided project for **Node DB 4** Module.
 
 ## Project Setup
 
-- [ ] **import** and clone this repository.
-- [ ] **CD into the folder** where you cloned **your version**.
-- [ ] type `npm i` to download dependencies.
-- [ ] type `npm run server` to start the API.
+- [X] **import** and clone this repository.
+- [X] **CD into the folder** where you cloned **your version**.
+- [X] type `npm i` to download dependencies.
+- [X] type `npm run server` to start the API.
 
 Please follow along as the instructor uses Knex migrations and seeding to create a multi-table database schema.
---
+++ data/migrations/20210204121304_create-tables.js
@@ -0,0 +1,38 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('zoos', table => {
+        table.increments();
+        table.string('zoo_name', 128).notNullable();
+        table.string('address', 128).notNullable().unique();
+    })
+    .createTable('species', table => {
+        table.increments();
+        table.string('species_name', 128).notNullable().unique();
+    })
+    .createTable('animals', table => {
+        table.increments();
+        table.string('animal_name', 128).notNullable();
+        table.integer('species_id')
+            .unsigned()
+            .notNullable()
+            .references('species.id')
+            .onDelete('CASCADE').onUpdate('CASCADE');
+    })
+    .createTable('zoo_animals', table => {
+        table.integer('zoo_id').unsigned().notNullable().references('zoos.id')
+        .onDelete('CASCADE').onUpdate('CASCADE');
+        table.integer('animal_id').unsigned().notNullable().references('animals.id')
+        .onDelete('CASCADE').onUpdate('CASCADE');
+        table.primary(['zoo_id', 'animal_id'])
+
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('zoo_animals')
+    .dropTableIfExists('animals')
+    .dropTableIfExists('species')
+    .dropTableIfExists('zoos')
+};
